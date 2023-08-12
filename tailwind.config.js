31c2f5890144aa2ba7914af128ef396eec1ee623
acq570 <marlon.misiak-hahnfeld@haw-hamburg.de>
2023-08-12 18:16:58 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Faustina: 'Faustina',
+      },
+      colors: {
+        GreyCustom: '#4D4D4D',
+        BeigeCustom: '#FFF4E0',
+        RedCustom: '#B46060',
+        OrangeCustom: '#FFBF9B'
+      },
+    },
+  },
+  plugins: [],
+}
+
