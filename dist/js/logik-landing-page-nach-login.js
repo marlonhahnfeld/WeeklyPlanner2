16801c3f5517ff39441e1d2f7ba32f776eb62552
UJ <ummar_j@outlook.com>
2023-08-15 23:40:36 +0200

    Grundstruktur Upper-Body implementiert. User Botton mit Dropdown Menü implementiert. Todos wurden hinzugefügt.

---
+++ dist/js/logik-landing-page-nach-login.js
@@ -0,0 +1,6 @@
+const dropdownButton = document.getElementById("Dropdown-Button");
+const dropdownMenu = document.getElementById("User-Dropdown-Menu");
+
+function showUserMenu() {
+  dropdownMenu.classList.toggle("hidden");
+}
