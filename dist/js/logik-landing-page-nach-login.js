3bc251e1743512fc4ae11062c55389d82317abe5
UJ <ummar_j@outlook.com>
2023-08-19 23:46:36 +0200

    Side-Menu-Bar hinzugefügt

--- dist/js/logik-landing-page-nach-login.js
+++ dist/js/logik-landing-page-nach-login.js
@@ -1,6 +1,11 @@
+const sideBarMenu = document.getElementById("Menu-Field");
 const dropdownButton = document.getElementById("Dropdown-Button");
 const dropdownMenu = document.getElementById("User-Dropdown-Menu");
 
 function showUserMenu() {
   dropdownMenu.classList.toggle("hidden");
+}
+
+function showSideMenu() {
+  sideBarMenu.classList.toggle("hidden");
 }
