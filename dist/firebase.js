7d752ac9e69cd8985db3f0f115e58bc30408fa98
marlon <marlon.misiak-hahnfeld@haw-hamburg.de>
2023-08-28 00:44:24 +0200

    auth funktioniert fast

--- dist/firebase.js
+++ dist/firebase.js
@@ -1,44 +1,51 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-analytics.js";
-  import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCPG1Sss9F8IUY91tiriI0V9DRuBadPG94",
-    authDomain: "weeklyplanner2-d6985.firebaseapp.com",
-    databaseURL: "https://weeklyplanner2-d6985-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "weeklyplanner2-d6985",
-    storageBucket: "weeklyplanner2-d6985.appspot.com",
-    messagingSenderId: "429065035897",
-    appId: "1:429065035897:web:6461c7c2a7421423d1f4a8",
-    measurementId: "G-5JJFFK5F6R"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGVWD9oopCpJxcurKz2R55FHwzNWuh9Cg",
+  authDomain: "weeklyplanner2-63de7.firebaseapp.com",
+  projectId: "weeklyplanner2-63de7",
+  storageBucket: "weeklyplanner2-63de7.appspot.com",
+  messagingSenderId: "742332640642",
+  appId: "1:742332640642:web:c387955abebf8de1d2f39b",
+  measurementId: "G-2RRFC79C48",
+};
 
-  const inputemail = document.getElementById("inputemail").textContent;
-  const inputpw = document.getElementById("inputpassword").textContent;
-  const inputpw2 = document.getElementById("inputpassword2").textContent; 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+function signUpUser() {
+  const inputemail = document.getElementById("inputemail").value;
+  const inputpw = document.getElementById("inputpassword").value;
+  const inputpw2 = document.getElementById("inputpassword2").value;
+  const btnregister = document.getElementById("btnregister");
   const passwordMatch = inputpw === inputpw2;
-
-  function signUpUser(){
-    if(passwordMatch){
-firebase.auth().createUserWithEmailAndPassword(inputemail,inputpw).catch(e=> console.log(e));
-    }else{
-      alert("Passwörter stimmen nicht überein!");
-    }
+  if (passwordMatch) {
+    console.log(inputemail + " " + inputpw);
+    createUserWithEmailAndPassword(auth, inputemail, inputpw)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
   }
-
-
-
-// ---------------------------------------------------------------------------------------------------------------------
-document.getElementById("btnregister").addEventListener("click", x);
-
-function x(){
-    console.log("test");
 }
+
+btnregister.addEventListener("click", signUpUser);
