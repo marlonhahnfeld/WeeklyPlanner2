fbcdcd0d83fca694b3cd8b48515fa9bf59447e34
acq570 <marlon.misiak-hahnfeld@haw-hamburg.de>
2023-09-05 21:39:56 +0200

    added tailwind colors and fontsizes + deleted useless index html

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,25 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        Faustina: 'Faustina',
-        
+        Faustina: "Faustina",
       },
       colors: {
-        GreyCustom: '#4D4D4D',
-        BeigeCustom: '#FFF4E0',
-        RedCustom: '#B46060',
-        OrangeCustom: '#FFBF9B'
+        GreyCustom: "#4D4D4D",
+        BeigeCustom: "#FFF4E0",
+        RedCustom: "#B46060",
+        OrangeCustom: "#FFBF9B",
+        SecondPrioCustom: "#3252AD",
+        PrioCustom: "#6989D0",
+      },
+      fontSize: {
+        h1: "3.5rem",
+        h2: "3rem",
+        h3: "2.5rem",
+        h4: "2rem",
+        h5: "1.5rem",
+        h6: "1rem",
       },
     },
   },
   plugins: [],
-}
-
+};
